43ecd4a9de4f4ba9a309a3f461057be633a6fde0
Blake Julander <13lakejulander@gmail.com>
2021-02-10 13:38:15 -0700

    commit tree 4941a2a9b5188f4c75ec0e7b88c4700391eecb3e

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,40 @@
 class Student
+  attr_accessor :name, :grade
+  attr_reader :id
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  def initialize(name, grade) 
+    @id = id
+    @name = name 
+    @grade = grade
+  end
+
+  def self.create_table
+    sql = <<-SQL 
+    create table students (
+      id integer primary key,
+      name text,
+      grade text
+    ) 
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "drop table students"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = "insert into students (name, grade) values (?, ?)"
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+
+
+  end
 end
